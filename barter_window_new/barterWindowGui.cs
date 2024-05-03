9f3ddfb18cd8085346f0e395e229c2f954c20d38
jgordon510 <jgordon.oakland@gmail.com>
2024-05-03 00:16:49 -0700

    Add project files.

---
+++ barter_window_new/barterWindowGui.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace barter_window
+{
+    public class BarterWindowGui : GuiDialog
+    {
+        public override string ToggleKeyCombinationCode => "barterwindow";
+        private static IInventory myInv;
+        private static InventoryGeneric theirInv;
+        private string myId;
+        private string theirId;
+        public BarterWindowGui(ICoreClientAPI capi, string otherId) : base(capi)
+        {
+            SetupDialog();
+            myId = capi.World.Player.PlayerUID;
+            theirId = otherId;
+            capi.Network.GetChannel("barter_change")
+                .SetMessageHandler<BarterChange>(OnClientReceiveChange);
+        }
+        private void SetupDialog()
+        {
+            ElementBounds dialogBounds = ElementStdBounds.AutosizedMainDialog.WithAlignment(EnumDialogArea.CenterMiddle);
+            ElementBounds bgBounds = ElementBounds.Fill.WithFixedPadding(GuiStyle.ElementToDialogPadding, 20.0);
+            ElementBounds insetBounds = ElementBounds.Fixed(10, 30, 220, 300);
+            ElementBounds slotGridBounds1 = ElementBounds.Fixed(10, 40, 200, 100);
+            ElementBounds textBounds1 = ElementBounds.Fixed(0, 10, 200, 20);
+            ElementBounds slotGridBounds2 = ElementBounds.Fixed(10, 190, 200, 100);
+            ElementBounds textBounds2 = ElementBounds.Fixed(0, 160, 210, 20);
+            ElementBounds buttonBounds = ElementBounds.Fixed(70, 350, 100, 40);
+            ElementBounds buttonTextBounds = ElementBounds.Fixed(90, 360, 100, 40);
+
+            bgBounds.BothSizing = ElementSizing.FitToChildren;
+            bgBounds.fixedHeight -= 20;
+            bgBounds.WithChildren(insetBounds);
+            bgBounds.WithChildren(buttonBounds);
+            bgBounds.WithChildren(buttonTextBounds);
+            insetBounds.WithChildren(slotGridBounds1);
+            insetBounds.WithChildren(slotGridBounds2);
+            insetBounds.WithChildren(textBounds1);
+            insetBounds.WithChildren(textBounds2);
+            if(theirInv==null) {
+                capi.Logger.Debug("NEW THEIR INVENTORY");
+                theirInv = new(8, null, null, capi);
+            } else
+            {
+                capi.Logger.Debug("EXISTING THEIR INVENTORY FOUND");
+            }
+            if(myInv == null) {
+                capi.Logger.Debug("no my inventory found");
+                //create the barter inventory on the first occurence of the gui
+                //we'll need to forcing it empty when the gui closes or the user logs
+                myInv = new InventoryGeneric(8, null, "barter", capi);
+                capi.World.Player.InventoryManager.OpenInventory(myInv);
+            }
+            
+            SingleComposer = capi.Gui.CreateCompo("barterwindow", dialogBounds)
+                .AddShadedDialogBG(bgBounds)
+                .AddDialogTitleBar("Barter Window", OnTitleBarCloseClicked)
+                .AddInset(insetBounds, 4, 0.85f)
+                .AddDynamicText("_their stuff_", CairoFont.WhiteSmallishText(), textBounds1, "theirstuff")
+                .AddItemSlotGrid(theirInv, DoNotSendPacket, 4, slotGridBounds1, "slots1") //callback goes nowhere
+                .AddDynamicText("_my stuff_", CairoFont.WhiteSmallishText(), textBounds2, "mystuff")
+                .AddItemSlotGrid(myInv, DoSendPacket, 4, slotGridBounds2, "slots2")
+                .AddSmallButton(Lang.Get("       "), OnOfferClick, buttonBounds, EnumButtonStyle.Normal, "offerbutton")
+                .AddDynamicText("Offer!", CairoFont.WhiteSmallishText(), buttonTextBounds, "buttontext")
+                .Compose()
+           ;
+           theirInv.PutLocked = true;
+           theirInv.TakeLocked = true;
+        }
+        private bool OnOfferClick()
+        {
+            GuiElementDynamicText textElem = SingleComposer.GetDynamicText("buttontext");
+            textElem.Text = "Retract!";
+            textElem.Bounds.WithFixedOffset(-10, 0);
+            capi.Logger.Debug(textElem.Text);
+            SingleComposer.ReCompose();
+            //this leads to the escrow process offer->retract (back to offer)->accept
+            return true;
+        }
+        private void OnTitleBarCloseClicked()
+        {
+            TryClose();
+            //deal with dumping contents of barter window
+        }
+        private void DoSendPacket(object p)
+        {
+            //this syncs my actual inventory
+            myInv.Open(capi.World.Player);
+            capi.Network.SendPacketClient(p);
+            //this reports it to the other player
+            capi.Network.GetChannel("barter_change").SendPacket(new BarterChange()
+            {
+                senderId = myId,
+                receiverId = theirId,
+                changes = null //this is derived on the server!
+            });
+        }
+
+        private void DoNotSendPacket(object p)
+        {
+            //We don't need to sync back the dummy grid.  This should never fire with put/takeLocked
+        }
+
+        private void OnClientReceiveChange(BarterChange change)
+        {
+            //temporarilly remove the locks, because without that we can't alter the inventory
+            theirInv.PutLocked = false;
+            theirInv.TakeLocked = false;
+            
+            for(int i = 0; i < 8 ; i++)
+            {
+                //remember: theirInv is a dummy - unsynced back to server.
+                //the strings are serialized on the server so they shouldn't be spoofed
+                ItemSlot theirSlot = theirInv[i];
+                //[0] = index; [1] = codestring; [2] = quantity as string
+                string[] changeSlot = change.changes[i].Split(";");
+                capi.Logger.Debug(change.changes[i]);
+                if (changeSlot[1] == "") {
+                    capi.Logger.Debug("EMPTY SLOT");
+                    //remove anything here - like gamelan
+                    int quant = 1;
+                    if (theirSlot?.Itemstack?.StackSize != null) quant = theirSlot.Itemstack.StackSize;
+                    theirSlot.TakeOut(quant);
+                    continue; 
+                }
+                capi.Logger.Debug("found: {0}" , changeSlot[1]);
+                Block newBlock = capi.World.GetBlock(new AssetLocation(changeSlot[1]));
+                theirSlot.Itemstack = new ItemStack(newBlock);
+                theirSlot.Itemstack.StackSize = Int32.Parse(changeSlot[2]);
+                theirSlot.MarkDirty(); //I think this does nothing here
+            }
+            //lock it back up
+            theirInv.PutLocked = true;
+            theirInv.TakeLocked = true;
+        }
+    }
+}
---
+++ barter_window_new/barter_windowModSystem.cs
@@ -0,0 +1,171 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.Server;
+using ProtoBuf;
+using System.Collections.Generic;
+using Vintagestory.API.Util;
+using Vintagestory.API.MathTools;
+using System.Reflection.Emit;
+
+namespace barter_window
+{
+    
+    public class barter_windowModSystem : ModSystem
+    {
+        private ICoreServerAPI sapi;
+        ICoreClientAPI capi;
+        GuiDialog dialog;
+        private Dictionary<string, InventoryBase> inventories;
+        public override void Start(ICoreAPI api)
+        {
+            api.Network.RegisterChannel("open_barter_request")
+                .RegisterMessageType(typeof(OpenRequest))
+                .RegisterMessageType(typeof(OpenResponse))
+            ;
+            api.Network.RegisterChannel("barter_change")
+                .RegisterMessageType(typeof(BarterChange))
+            ;
+            inventories = new();
+        }
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+            base.StartClientSide(api);
+            capi = api;
+            capi.Input.RegisterHotKey("barterwindow", "A barter window.", GlKeys.B, HotkeyType.GUIOrOtherControls);
+            capi.Input.SetHotKeyHandler("barterwindow", ToggleGui);
+            capi.Network.GetChannel("open_barter_request")
+                .SetMessageHandler<OpenResponse>(OnReceiveOpenResponse);
+        }
+        
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            api.Logger.Debug("SERVER START LEADERSTATS");
+            sapi = api;
+            api.Network.GetChannel("open_barter_request")
+                .SetMessageHandler<OpenRequest>(OnClientRequestOpen);
+            api.Network.GetChannel("barter_change")
+                .SetMessageHandler<BarterChange>(OnClientAnnounceChange);
+            sapi.Event.PlayerNowPlaying += OnPlayerNowPlaying;
+        }
+
+        public void OnPlayerNowPlaying(IServerPlayer byPlayer) {
+            InventoryBase inv = new InventoryGeneric(8, null, "barter", sapi);
+            byPlayer.InventoryManager.OpenInventory(inv);
+            //empty anything that got saved; todo move their barter stuff back when the gui gets closed or they log off
+            foreach (ItemSlot slot in inv) 
+            {
+                int quant = 1;
+                if (slot?.Itemstack?.StackSize != null) quant = slot.Itemstack.StackSize;
+                slot.TakeOut(quant);
+            }
+            inventories.Add(byPlayer.PlayerUID, inv);
+        }
+
+        private bool ToggleGui(KeyCombination comb)
+        {
+            if (dialog != null && dialog.IsOpened()) dialog.TryClose();
+            else
+            {
+                capi.Network.GetChannel("open_barter_request").SendPacket(new OpenRequest()
+                {
+                    uid = capi.World.Player.PlayerUID
+                });
+            }
+            return true;
+        }
+        
+        private void OnClientRequestOpen(IPlayer fromPlayer, OpenRequest request)
+        {
+            sapi.Logger.Debug("OPENING INVENTORY");
+
+            sapi.Network.GetChannel("open_barter_request").SendPacket(new OpenResponse()
+            {
+                invId = "barter"
+            }, fromPlayer as IServerPlayer);
+        }
+        
+        private void OnReceiveOpenResponse(OpenResponse networkMessage)
+        {
+            //we need to verify that we're looking at another player to open the dialog
+            capi.Logger.Debug("client received {0}", networkMessage.invId);
+            if (capi.World?.Player?.CurrentEntitySelection?.Entity == null) return;
+            capi.Logger.Debug("NAME {0}",capi.World.Player.CurrentEntitySelection.Entity.GetName());
+            if (capi.World.Player.CurrentEntitySelection.Entity is not EntityPlayer) return;
+            EntityPlayer player = capi.World.Player.CurrentEntitySelection.Entity as EntityPlayer;
+            //spoofing this would allow you to peek inside other player's barter inventories
+            //seems like non-issue
+            dialog = new BarterWindowGui(capi, player.PlayerUID); 
+            dialog.TryOpen();
+            // todo we also need to broadcast a message to the other player to open their window
+        }
+
+
+        private void OnClientAnnounceChange(IPlayer fromPlayer, BarterChange change)
+        {
+            sapi.Logger.Debug("received change");
+            InventoryBase inv = inventories.Get(fromPlayer.PlayerUID);
+            if (inv == null) {
+                sapi.Logger.Debug("THERE WAS NO INVENTORY FOUND WTF");
+                return; 
+            }
+            else sapi.Logger.Debug("HOORARY!");
+            //we need to serialize the inventory
+            string[] changeList = new string[8];
+            int i = 0;
+            foreach (ItemSlot item in inv)
+            {
+                if (item.Empty)
+                {
+                    changeList[i] = ";;;";
+                    i++;
+                    continue;
+                } 
+                string c;
+                //still need todo tools
+                if (item?.Itemstack?.Item != null)
+                {
+                    c = item.Itemstack.Item.Code.ToString();
+                }
+                else
+                {
+                    c = item.Itemstack.Block.Code.ToString();
+                }
+
+                sapi.Logger.Debug("FOUND ITEM: {0}", c);
+                string ss = item.Itemstack.StackSize.ToString();
+
+                changeList[i] = i.ToString() + ";" + c + ";" + ss;
+                sapi.Logger.Debug(changeList[i]);
+                i++;
+            }
+            IPlayer receiver = sapi.World.PlayerByUid(change.receiverId);
+            sapi.Network.GetChannel("barter_change").SendPacket(new BarterChange()
+            {
+                senderId = change.senderId,
+                receiverId = change.receiverId,
+                changes = changeList, //this is the real deal; generated server side
+            }, receiver as IServerPlayer);
+        }
+    }
+    [ProtoContract(ImplicitFields = ImplicitFields.AllPublic)]
+    public class OpenRequest
+    {
+        public string uid;
+    }
+
+    [ProtoContract(ImplicitFields = ImplicitFields.AllPublic)]
+    public class OpenResponse
+    {
+        public string invId;
+    }
+
+    [ProtoContract(ImplicitFields = ImplicitFields.AllPublic)]
+    public class BarterChange
+    {
+        public string senderId;
+        public string receiverId;
+        public string[] changes; //this is only sent on server calls; client sends null
+    }
+}
